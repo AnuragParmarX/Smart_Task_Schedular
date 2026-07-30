9e124a0a62151c22515e1d1156f331dc33198a63
AnuragParmarX <anu17parmar2004@gmail.com>
2026-07-30 13:15:02 +0530

    Create TaskRepository interface for Task entity

---
+++ TaskReposiotry.java
@@ -0,0 +1,8 @@
+package com.anurag.scheduler.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.anurag.scheduler.entity.Task;
+
+public interface TaskRepository extends JpaRepository<Task,Long>{
+
+}
