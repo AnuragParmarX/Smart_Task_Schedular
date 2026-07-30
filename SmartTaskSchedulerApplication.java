b9f044a2f51cd1c53ad87845848c4096ac520447
AnuragParmarX <anu17parmar2004@gmail.com>
2026-07-30 13:10:34 +0530

    Create SmartTaskSchedulerApplication class

---
+++ SmartTaskSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.anurag.scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTaskSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartTaskSchedulerApplication.class, args);
+    }
+
+}
