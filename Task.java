1bcf73b9c7dd4864b2d9a368a49ca2512d117a28
AnuragParmarX <anu17parmar2004@gmail.com>
2026-07-30 13:12:29 +0530

    Create Task.java

---
+++ Task.java
@@ -0,0 +1,63 @@
+package com.anurag.scheduler.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private String priority;
+
+    private String status;
+
+    private LocalDate dueDate;
+
+    @ManyToOne
+    private Category category;
+
+    @ManyToOne
+    private User user;
+
+    public Task(){}
+
+    public Long getId(){ return id; }
+
+    public void setId(Long id){ this.id=id; }
+
+    public String getTitle(){ return title; }
+
+    public void setTitle(String title){ this.title=title; }
+
+    public String getDescription(){ return description; }
+
+    public void setDescription(String description){ this.description=description; }
+
+    public String getPriority(){ return priority; }
+
+    public void setPriority(String priority){ this.priority=priority; }
+
+    public String getStatus(){ return status; }
+
+    public void setStatus(String status){ this.status=status; }
+
+    public LocalDate getDueDate(){ return dueDate; }
+
+    public void setDueDate(LocalDate dueDate){ this.dueDate=dueDate; }
+
+    public Category getCategory(){ return category; }
+
+    public void setCategory(Category category){ this.category=category; }
+
+    public User getUser(){ return user; }
+
+    public void setUser(User user){ this.user=user; }
+
+}
